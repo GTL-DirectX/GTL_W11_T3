86ba05ecad20089f81f1dcd1e9fdc96f809c7132
wannaBpark <leehun456@hanmail.net>
2025-05-13 00:49:43 +0900

    Fixed - Outliner에 skeletalmesh component 관찰되도록 ACharacter/World 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/World/World.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/World/World.cpp
@@ -244,6 +244,8 @@ AActor* UWorld::SpawnActor(UClass* InClass, FName InActorName)
         // Actor->InitializeComponents();
         ActiveLevel->Actors.Add(NewActor);
         PendingBeginPlayActors.Add(NewActor);
+        NewActor->PostSpawnInitialize();
+
         return NewActor;
     }
     
