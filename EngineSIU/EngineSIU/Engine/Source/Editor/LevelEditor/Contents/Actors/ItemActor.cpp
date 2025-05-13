a77977d9267fb31adbeca3fdf63b02ce77737175
wannaBpark <leehun456@hanmail.net>
2025-05-13 18:02:53 +0900

    이거뭐지

---
+++ EngineSIU/EngineSIU/Engine/Source/Editor/LevelEditor/Contents/Actors/ItemActor.cpp
@@ -0,0 +1,67 @@
+
+#include "ItemActor.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/FObjLoader.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Engine/FFbxLoader.h"
+
+AItemActor::AItemActor()
+{
+    MeshComponent = AddComponent<USkeletalMeshComponent>(FName("MeshComponent_0"));
+    
+    // auto mesh = FFbxLoader::GetSkeletalMesh("Contents/dragon/Dragon 2.5_fbx.fbx");
+    // MeshComponent->SetSkeletalMesh(mesh);
+}
+
+void AItemActor::PostSpawnInitialize()
+{
+    AActor::PostSpawnInitialize();
+    /*
+    MeshComponent = AddComponent<USkeletalMeshComponent>(FName("MeshComponent_0"));
+    auto mesh = FFbxLoader::GetFbxObject("Contents/X Bot.fbx");
+    MeshComponent->SetSkeletalMesh(mesh);
+    // MeshComponent->SetSkeletalMesh(FFbxManager::GetSkeletalMesh(L"Contents/55-rp_nathan_animated_003_walking_fbx/rp_nathan_animated_003_walking.fbx"));
+    //MeshComponent->SetSkeletalMesh(FFbxManager::GetSkeletalMesh(L"Contents/hand/girl.fbx"));
+    //MeshComponent->SetSkeletalMesh(FFbxManager::GetSkeletalMesh(L"Contents/suzanne.fbx"));
+    MeshComponent->SetupAttachment(RootComponent);
+    */
+}
+
+void AItemActor::BeginPlay()
+{
+    AActor::BeginPlay();
+
+    SetHidden(false);
+
+    OnActorBeginOverlap.AddLambda(
+        [this](AActor* OverlappedActor, AActor* OtherActor)
+        {
+            ActorBeginOverlap(OverlappedActor, OtherActor);
+        }
+    );
+}
+
+void AItemActor::Tick(float DeltaTime)
+{
+    AActor::Tick(DeltaTime);
+
+    ElapsedTime += DeltaTime;
+
+    if (UStaticMeshComponent* MeshComp = GetComponentByClass<UStaticMeshComponent>())
+    {
+        MeshComp->SetRelativeRotation(FRotator(0.f, ElapsedTime * Speed, 0.f));
+        MeshComp->SetRelativeLocation(FVector(0.f, 0.f, FMath::Sin(ElapsedTime * FloatingFrequency) * FloatingHeight));       
+    }
+}
+
+void AItemActor::Reset()
+{
+    SetHidden(false);
+}
+
+void AItemActor::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+    
+}
