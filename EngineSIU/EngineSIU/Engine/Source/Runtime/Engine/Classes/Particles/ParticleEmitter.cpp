b61b6d7631ec9d0caf09ea42230dd2e9be322598
Joochan <joochan123123@gmail.com>
2025-05-17 15:23:50 +0900

    feat: ParticleModule, ParticleSystem, Emitter 클래스 추가.

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleEmitter.cpp
@@ -0,0 +1,12 @@
+#include "ParticleEmitter.h"
+
+#include "ParticleHelper.h"
+
+void UParticleEmitter::CacheEmitterModuleInfo()
+{
+    ParticleSize = sizeof(FBaseParticle);
+
+    /*
+    ........
+    */
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleModuleLocationBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ParticleModule.h"
+
+class UParticleModuleLocationBase : public UParticleModule
+{
+    DECLARE_CLASS(UParticleModuleLocationBase, UParticleModule)
+
+public:
+    UParticleModuleLocationBase() = default;
+    
+};
