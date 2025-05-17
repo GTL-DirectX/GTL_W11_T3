7299b923f1e93a1623292e26a0c8359182eee5e3
Joochan <joochan123123@gmail.com>
2025-05-17 20:54:08 +0900

    feat: Particle시스템용 Actor 추가.

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleActor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+
+class AParticleActor : public AActor
+{
+    DECLARE_CLASS(AParticleActor, AActor)
+
+public:
+    AParticleActor() = default;
+
+    virtual void PostSpawnInitialize() override;
+    virtual UObject* Duplicate(UObject* InOuter) override;
+
+private:
+    class UParticleSystemComponent* ParticleSystemComponent;
+
+};
