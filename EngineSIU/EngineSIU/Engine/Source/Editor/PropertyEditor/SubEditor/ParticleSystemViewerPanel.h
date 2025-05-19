39c64f3940aa74ba000ebcf87f13dc0c1cdd6a10
Jay <seojoongwon2020@gmail.com>
2025-05-19 19:38:55 +0900

    Add: ParticleSystemViewerPanel::RenderProperties 템플릿 추가

--- EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/SubEditor/ParticleSystemViewerPanel.h
+++ EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/SubEditor/ParticleSystemViewerPanel.h
@@ -3,6 +3,8 @@
 #include "Particles/ParticleSystemComponent.h"
 #include "UnrealEd/EditorPanel.h"
 
+#include "PropertyEditor/ImGuiInspector.h"
+
 class UParticleSystem;
 
 class ParticleSystemViewerPanel : public UEditorPanel
@@ -21,6 +23,9 @@ private:
     void RenderDetails();
     void RenderCurveEditor();
 
+    template <class T>
+    void RenderProperties(T* Obj);
+
 private:
     float Width = 800, Height = 600;
 
@@ -31,3 +36,21 @@ private:
     int SelectedEmitterIndex = -1;
 };
 
+
+template<typename T>
+void ParticleSystemViewerPanel::RenderProperties(T* Obj)
+{
+    if (!Obj)
+    {
+        return;
+    }
+    UObject* UObjectPtr = Cast<UObject>(Obj);
+    UClass* Cls = UObjectPtr->GetClass();
+
+    // 등록된 모든 UField 를 순회하면서 DrawFieldEditor 호출
+    Cls->ForEachField([&](UField* Field)
+        {
+            ImGuiInspector::DrawFieldEditor(Field, UObjectPtr);
+            ImGui::Spacing();
+        });
+}
