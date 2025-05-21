501fafb5d4a18521bfaf917041d8e9ef59b8df07
wannaBpark <leehun456@hanmail.net>
2025-05-21 16:10:17 +0900

    Chores Size

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/ParticleModules_Size.cpp
@@ -0,0 +1,34 @@
+#include "ParticleModuleSize.h"
+#include "UObject/Casts.h"
+#include "ParticleEmitterInstances.h"
+#include "Distributions/DistributionVectorUniform.h"
+#include "UObject/ObjectFactory.h"
+
+UParticleModuleSize::UParticleModuleSize()
+{
+    bEnabled = true;
+    bSpawnModule = true;
+    bUpdateModule = false;
+}
+
+
+void UParticleModuleSize::PostInitProperties()
+{
+    Super::PostInitProperties();
+    StartSize.Distribution = FObjectFactory::ConstructObject<UDistributionVectorUniform>(this);
+    if (auto* Dist = Cast<UDistributionVectorUniform>(StartSize.Distribution))
+    {
+        Dist->Min = FVector(1.f, 1.f, 1.f);
+        Dist->Max = FVector(1.f, 1.f, 1.f);
+    }
+}
+
+void UParticleModuleSize::Spawn(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FBaseParticle* ParticleBase)
+{
+    SPAWN_INIT
+    FVector Size = StartSize.GetValue(Owner->EmitterTime, Cast<UObject>(Owner->Component), 0);
+    Particle.Size += Size;
+    Particle.BaseSize += Size;
+}
+
+
